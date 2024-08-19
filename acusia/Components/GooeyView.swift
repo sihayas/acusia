9466fb478965723cb6d2a7783c31b76a33011e29
cora type <coratype@gmail.com>
2024-08-19 04:42:01 -0400

    dynamic frame gooey

--- acusia/Components/GooeyView.swift
+++ acusia/Components/GooeyView.swift
@@ -9,90 +9,87 @@ import SwiftUI
 
 struct GooeyView: View {
     @State private var offset: CGSize = .zero
-    private let circleDiameter = 90.0
-    private let deviceWidth = UIScreen.main.bounds.width
-    private let deviceHeight = UIScreen.main.bounds.height
-    
-    private var initialX: Double {
-        deviceWidth / 2.0
-    }
-    
-    private var initialY: Double {
-        deviceHeight / 2.0
-    }
-    
+    @State private var contentSize: CGSize = .zero // State to hold the measured size of the VStack
+
     var body: some View {
         ZStack {
-            Canvas { context, _ in
-                let textBubble = context.resolveSymbol(id: 0)!
-//                let artifactCard = context.resolveSymbol(id: 1)!
-                
-                // The core of the effect is essentially blurring the shapes
-                // and then applying an alpha threshold which bumps the opacity
-                // of the parts of the shape that have been blurred below the
-                // threshold to 1, creating the gooey effect.
-//                context.addFilter(.alphaThreshold(min: 0.5, color: .black))
-//                context.addFilter(.blur(radius: 6))
-                
-                context.drawLayer { ctx in
-                    ctx.draw(textBubble, at: CGPoint(x: initialX, y: initialY))
-//                    ctx.draw(artifactCard, at: CGPoint(x: initialX, y: initialY))
-                }
-            } symbols: {
-                VStack(alignment: .leading, spacing: 16) {
-                    RoundedRectangle(cornerRadius: 32)
-                        .frame(width: 196, height: 196)
-                    Text("hihihihihihihihihihihihihi")
-                        .font(.system(size: 16, weight: .regular))
-                        .foregroundColor(.white)
-                        .padding(.horizontal, 14)
-                        .padding(.vertical, 10)
-                        .background(.black, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
-                    EmptyView()
-                }
-                .frame(maxWidth: .infinity, alignment: .bottomLeading)
-                .padding()
-                .border(Color.blue, width: 4)
-                .offset(x: offset.width, y: offset.height)
-                .tag(0)
+            VStack(alignment: .leading, spacing: 16) {
+                RoundedRectangle(cornerRadius: 32)
+                    .fill(.clear)
+                    .frame(width: 196, height: 196)
+                Text("Very reminiscent of The Ascension if I'm being entirely honest, primarily in that it feels equal parts trailblazing and perplexing.")
+                    .font(.system(size: 16, weight: .regular))
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 14)
+                    .padding(.vertical, 10)
+//                    .background(.clear, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
+                    .offset(x: offset.width, y: offset.height)
             }
-            .gesture(
-                DragGesture()
-                    .onChanged { value in
-                        withAnimation(.spring()) {
-                            offset = value.translation
-                        }
+            .frame(maxWidth: .infinity, alignment: .bottomLeading)
+            .padding(24)
+            .background(
+                GeometryReader { geometry in
+                    Color.clear.onAppear {
+                        contentSize = geometry.size
+                        print("contentSize: \(contentSize)")
                     }
-                    .onEnded { _ in
-                        withAnimation(.spring()) {
-                            offset = .zero
-                        }
-                    }
+                }
             )
-            
-            // Content Overlay
-            // Because of the blur of the blob effect, content needs to be rendered 1:1
-            // on top of the gooey effect shapes. The shapes in the canvas will take care of
-            // the gooey effect, while the shapes here will serve as the frame/overlay.
-//            RoundedRectangle(cornerRadius: 32)
-//                .fill(Color.clear)
-//                .frame(width: 196, height: 196)
-//                .border(Color.green, width: 1)
-//                .offset(x: offset.width, y: offset.height)
-//                .allowsHitTesting(false)
-//            
-//            Text("hi there hi there hi there hi there hi there")
-//                .font(.system(size: 16, weight: .regular))
-//                .foregroundColor(.white)
-//                .padding(.horizontal, 14)
-//                .padding(.vertical, 10)
-//                .border(Color.green, width: 1)
-//                .background(.clear, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
+            .allowsHitTesting(false)
+            .zIndex(1)
+            .tag(0)
+
+            if contentSize != .zero {
+                Canvas { context, _ in
+                    // Link the "symbol" by id + tag.
+                    let container = context.resolveSymbol(id: 0)!
+
+                    // The core of the effect is essentially blurring the shapes
+                    // and then applying an alpha threshold which bumps the opacity
+                    // of the parts of the shape that have been blurred below the
+                    // threshold to 1, creating the gooey effect.
+                    context.addFilter(.alphaThreshold(min: 0.5, color: .black))
+                    context.addFilter(.blur(radius: 6))
+
+                    context.drawLayer { ctx in
+                        ctx.draw(container, at: CGPoint(x: contentSize.width / 2, y: contentSize.height / 2))
+                    }
+                } symbols: {
+                    VStack(alignment: .leading, spacing: 16) {
+                        RoundedRectangle(cornerRadius: 32)
+                            .frame(width: 196, height: 196)
+                        Text("Very reminiscent of The Ascension if I'm being entirely honest, primarily in that it feels equal parts trailblazing and perplexing.")
+                            .font(.system(size: 16, weight: .regular))
+                            .foregroundColor(.white)
+                            .padding(.horizontal, 14)
+                            .padding(.vertical, 10)
+                            .background(.black, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
+                            .offset(x: offset.width, y: offset.height)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .bottomLeading)
+                    .padding(24)
+                    .tag(0)
+                }
+                .frame(maxWidth: contentSize.width, maxHeight: contentSize.height)
+                .border(Color.red, width: 2)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            withAnimation(.spring()) {
+                                offset = value.translation
+                            }
+                        }
+                        .onEnded { _ in
+                            withAnimation(.spring()) {
+                                offset = .zero
+                            }
+                        }
+                )
+            }
         }
-        .ignoresSafeArea(.container, edges: [.top, .bottom])
-        .frame(width: .infinity, height: .infinity)
         .background(Color.white)
-        .border(Color.red, width: 1)
+        .frame(maxWidth: .infinity)
+        .border(Color.black, width: 1)
     }
 }
 
