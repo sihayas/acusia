7729138ec4941c382d14a79f3440f727efbfc463
cora type <coratype@gmail.com>
2024-09-27 06:06:59 -0400

    aperture prototype improvements

--- acusia/Components/AperturePrototype.swift
+++ acusia/Components/AperturePrototype.swift
@@ -1,122 +1,189 @@
 import SwiftUI
 
 struct ContentView: View {
-    let centerWidth = UIScreen.main.bounds.width / 2
-    let centerHeight = UIScreen.main.bounds.height / 2
-    
-    @State var rightPosition = CGSize(width: 0, height: 0)
-    @State var leftPosition = CGSize(width: 0, height: 0)
-    @State var shrinkRight: Bool = false
     @State var blurRadius: CGFloat = 4
     
+    @State var leftPos = CGSize(width: 0, height: 0)
+    @State var midPos = CGSize(width: 0, height: 0)
+    @State var rightPos = CGSize(width: 0, height: 0)
+    
+    @State var collapseLeft: Bool = false
+    
+    // Middle bar
+    @State var expandSearch: Bool = false
+    @State var expandReply: Bool = false
+    
     var body: some View {
-        ZStack {
-            Canvas { context, _ in
-                // Resolve symbols to draw
-                let firstCircle = context.resolveSymbol(id: 0)!
-                let secondCircle = context.resolveSymbol(id: 1)!
-                let thirdCircle = context.resolveSymbol(id: 2)!
-                
-                // Add filters
-                context.addFilter(.alphaThreshold(min: 0.5, color: .black))
-                context.addFilter(.blur(radius: blurRadius))
-                
-                context.drawLayer { context2 in
-                    context2.draw(firstCircle, at: CGPoint(x: centerWidth, y: centerHeight))
-                    context2.draw(secondCircle, at: CGPoint(x: centerWidth, y: centerHeight))
-                    context2.draw(thirdCircle, at: CGPoint(x: centerWidth, y: centerHeight))
-                }
-            } symbols: {
-                // Left
-                Capsule()
-                    .frame(width: 128, height: 36)
-                    .offset(x: leftPosition.width, y: leftPosition.height)
-                    .tag(2)
-
-                Capsule()
-                    .frame(width: 128, height: 36)
-                    .tag(0)
-                
-                // Right
-                Capsule()
-                    .frame(width: shrinkRight ? 36 : 128, height: 36)
-                    .offset(x: rightPosition.width, y: rightPosition.height)
-                    .frame(width: 128, height: 36) // Prevent layout shift.
-                    .tag(1)
-            }
+        
+        GeometryReader { geometry in
+            let leftDimensions: CGSize = collapseLeft
+            ? CGSize(width: 36, height: 36)
+            : CGSize(width: 128, height: 36)
             
-            VStack {
-                Spacer()
-                HStack {
-                    // Button to animate the first circle to the left
-                    Button(action: {
-                        withAnimation(Animation.interpolatingSpring(
-                            mass: 1.0,
-                            stiffness: pow(2 * .pi / 0.5, 2), // Response 0.5
-                            damping: 4 * .pi * 0.7 / 0.5, // Damping 0.7
-                            initialVelocity: 10.0
-                        )) {
-                            self.blurRadius = 0
-                            self.leftPosition = CGSize(width: -46, height: 0)
-                        } completion: {
-                            blurRadius = 0
-                        }
-                    }) {
-                        Image(systemName: "chevron.left")
-                            .font(.system(size: 19))
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color.blue)
-                            .clipShape(Circle())
-                    }
+            let midDimensions: CGSize = expandSearch
+            ? CGSize(width: geometry.size.width - 48, height: 48)
+            : CGSize(width: 128, height: 36)
+            
+            let rightDimensions: CGSize = expandReply
+            ? CGSize(width: geometry.size.width - 48 - 36 - 16, height: 36)
+            : CGSize(width: 128, height: 36)
+            
+            ZStack {
+                // MARK: Canvas
+                
+                Canvas { context, size in
+                    let firstCircle = context.resolveSymbol(id: 0)!
+                    let secondCircle = context.resolveSymbol(id: 1)!
+                    let thirdCircle = context.resolveSymbol(id: 2)!
                     
-                    // Reset button
-                    Button(action: {
-                        withAnimation(Animation.interpolatingSpring(
-                            mass: 1.0,
-                            stiffness: pow(2 * .pi / 0.5, 2), // Response 0.5
-                            damping: 4 * .pi * 0.7 / 0.5, // Damping 0.7
-                            initialVelocity: 0.0
-                        )) {
-                            self.blurRadius = 4
-                            self.rightPosition = .zero
-                            self.leftPosition = .zero
-                            self.shrinkRight = false
-                        } completion: {
-                            blurRadius = 0
-                        }
-                    }) {
-                        Image(systemName: "arrow.clockwise")
-                            .font(.system(size: 14))
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color.blue)
-                            .clipShape(Circle())
-                    }
+                    // Add filters
+                    context.addFilter(.alphaThreshold(min: 0.5, color: .black))
+                    context.addFilter(.blur(radius: blurRadius))
                     
-                    // Button to animate the second circle to the right
-                    Button(action: {
-                        withAnimation(Animation.interpolatingSpring(
-                            mass: 1.0,
-                            stiffness: pow(2 * .pi / 0.5, 2), // Response 0.5
-                            damping: 4 * .pi * 0.7 / 0.5, // Damping 0.7
-                            initialVelocity: 10.0
-                        )) {
-                            self.blurRadius = 4
-                            self.rightPosition = CGSize(width: (centerWidth - 126) + 16, height: 0)
-                            self.shrinkRight = true
-                        } completion: {
-                            blurRadius = 0
-                        }
-                    }) {
-                        Image(systemName: "chevron.right")
-                            .font(.system(size: 19))
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(Color.blue)
-                            .clipShape(Circle())
+                    // Calculate the center of the canvas
+                    let center = CGPoint(x: size.width / 2, y: size.height / 2)
+                    
+                    // Draw symbols at the center
+                    context.drawLayer { context2 in
+                        context2.draw(firstCircle, at: center)
+                        context2.draw(secondCircle, at: center)
+                        context2.draw(thirdCircle, at: center)
                     }
+                } symbols: {
+                    Capsule()
+                        .frame(width: leftDimensions.width, height: leftDimensions.height)
+                        .offset(x: leftPos.width, y: leftPos.height)
+                        .frame(width: geometry.size.width - 48, height: 48, alignment: .bottom)
+                        .tag(1)
 
+                    RoundedRectangle(cornerRadius: 18, style: .continuous)
+                        .frame(width: midDimensions.width, height: midDimensions.height)
+                        .offset(x: midPos.width, y: midPos.height)
+                        .frame(width: geometry.size.width - 48, height: 48, alignment: .bottom)
+                        .tag(0)
+                    
+                    RoundedRectangle(cornerRadius: 18, style: .continuous)
+                        .frame(width: rightDimensions.width, height: rightDimensions.height)
+                        .offset(x: rightPos.width, y: rightPos.height)
+                        .frame(width: geometry.size.width - 48, height: 48, alignment: .bottom)
+                        .tag(2)
+                }
+                
+                // MARK: Content Overlay
+
+                Capsule()
+                    .frame(width: 128, height: 36)
+                    .offset(x: rightPos.width, y: rightPos.height)
+                    .foregroundColor(.clear)
+                
+                Capsule()
+                    .frame(width: 128, height: 36)
+                    .foregroundColor(.clear)
+                
+                Capsule()
+                    .frame(width: collapseLeft ? 36 : 128, height: 36)
+                    .offset(x: leftPos.width, y: leftPos.height)
+                    .foregroundColor(.clear)
+                
+                // MARK: Controls
+                
+                VStack {
+                    Spacer()
+                    HStack {
+                        // Button to animate the first circle to the left
+                        Button(action: {
+                            withAnimation(.interpolatingSpring(
+                                mass: 1.0,
+                                stiffness: pow(2 * .pi / 0.5, 2), // Response 0.5
+                                damping: 4 * .pi * 0.7 / 0.5, // Damping 0.7
+                                initialVelocity: 10.0
+                            )) {
+                                self.blurRadius = 4
+                                self.leftPos = CGSize(width: -(geometry.size.width / 2 - 126) - 16, height: 0)
+                                self.collapseLeft = true
+                            } completion: {
+                                blurRadius = 0
+                            }
+                        }) {
+                            Image(systemName: "chevron.left")
+                                .font(.system(size: 19))
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.blue)
+                                .clipShape(Circle())
+                        }
+                        
+                        // Reset button
+                        Button(action: {
+                            withAnimation(.interpolatingSpring(
+                                mass: 1.0,
+                                stiffness: pow(2 * .pi / 0.5, 2), // Response 0.5
+                                damping: 4 * .pi * 0.7 / 0.5, // Damping 0.7
+                                initialVelocity: 0.0
+                            )) {
+                                self.blurRadius = 4
+                                self.leftPos = .zero
+                                self.rightPos = .zero
+                                self.midPos = .zero
+                                self.collapseLeft = false
+                                self.expandSearch = false
+                                self.expandReply = false
+                            } completion: {
+                                blurRadius = 0
+                            }
+                        }) {
+                            Image(systemName: "arrow.clockwise")
+                                .font(.system(size: 14))
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.yellow)
+                                .clipShape(Circle())
+                        }
+                        
+                        // Button to expand the search bar
+                        Button(action: {
+                            withAnimation(.interpolatingSpring(
+                                mass: 1.0,
+                                stiffness: pow(2 * .pi / 0.5, 2), // Response 0.5
+                                damping: 4 * .pi * 0.7 / 0.5, // Damping 0.7
+                                initialVelocity: 10.0
+                            )) {
+                                self.expandSearch = true
+                            }
+                        }) {
+                            Image(systemName: "magnifyingglass")
+                                .font(.system(size: 13))
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.blue)
+                                .clipShape(Circle())
+                        }
+                        
+                        // Button to expand the reply button
+                        Button(action: {
+                            withAnimation(.interpolatingSpring(
+                                mass: 1.0,
+                                stiffness: pow(2 * .pi / 0.5, 2), // Response 0.5
+                                damping: 4 * .pi * 0.7 / 0.5, // Damping 0.7
+                                initialVelocity: 10.0
+                            )) {
+                                self.blurRadius = 4
+                                self.leftPos = CGSize(width: -(geometry.size.width / 2 - 18) + 24, height: 0)
+                                self.rightPos = CGSize(width: 24, height: 0)
+                                self.collapseLeft = true
+                                self.expandReply = true
+                            } completion: {
+                                blurRadius = 0
+                            }
+                        }) {
+                            Image(systemName: "arrowshape.turn.up.right.fill")
+                                .font(.system(size: 17))
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.blue)
+                                .clipShape(Circle())
+                        }
+                    }
                 }
             }
         }
@@ -127,8 +194,7 @@ struct ContentView: View {
     ContentView()
 }
 
-
-//VStack(alignment: .leading, spacing: 12) {
+// VStack(alignment: .leading, spacing: 12) {
 //    RoundedRectangle(cornerRadius: 32)
 //        .fill(.black)
 //        .frame(width: 196, height: 196)
@@ -140,7 +206,7 @@ struct ContentView: View {
 //        .background(.black, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
 //        .scaleEffect(animateScale ? 1 : 0, anchor: .top)
 //        .offset(y: animateOffset ? 0 : -16)
-//}
-//.frame(maxWidth: .infinity, alignment: .bottomLeading)
-//.padding(12)
-//.tag(0)
+// }
+// .frame(maxWidth: .infinity, alignment: .bottomLeading)
+// .padding(12)
+// .tag(0)
